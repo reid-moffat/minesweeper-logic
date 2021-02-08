852065eb146ad182f5c99a9d37cc4ee28f650f13
Reid Moffat <reid.moffat9@gmail.com>
2021-02-08 02:12:26 -0500

    Replaced -1 and -2 with U and M for ease of understanding

--- predefined_states.py
+++ predefined_states.py
@@ -5,106 +5,104 @@ Below are some initial 5x5 states that are used for testing the code
 -See '4th state example.PNG' for a real minesweeper state example
 
 Notation explanation:
-u: unknown square (middle square is always -1 because it is being solved for)
-m: known mine (in a real game, this would be a flag)
+U: unknown square (middle square is always unknown because it is being solved for)
+M: known mine (in a real game, this would be a flag)
 0 <= n <= 8: uncovered spot with n adjacent bombs (includes diagonals)
 '''
 
-# TODO: replace all -1s with u and -2s with m in the states
-
 # Makes the states easier to understand, while still allowing for arithmetic
 # comparisons to check the state
-u = -1
-m = -2
+U = -1  # Unknown
+M = -2  # Mine
 
 state_list = [
     # State 1 is a mine
     [
-        [1, -1, -1, -1, -1],
-        [1, -1, -1, -1, 2],
-        [2, -2, -1, 3, 2],
-        [3, -2, 5, -2, 1],
-        [3, -2, 3, 1, 1]
+        [1, U, U, U, U],
+        [1, U, U, U, 2],
+        [2, M, U, 3, 2],
+        [3, M, 5, M, 1],
+        [3, M, 3, 1, 1]
     ],
     "mine",
 
     # State 2 is a mine
     [
-        [2, 1, 1, 2, -2],
-        [1, 2, -1, -1, -1],
-        [1, 3, -1, 1, -1],
-        [1, -2, -1, -1, 3],
-        [1, 1, 2, 2, -1]
+        [2, 1, 1, 2, M],
+        [1, 2, U, U, U],
+        [1, 3, U, 1, U],
+        [1, M, U, U, 3],
+        [1, 1, 2, 2, U]
     ],
     "mine",
 
     # State 3 is safe
     [
-        [-1, -1, -1, -1, -1],
-        [-1, -1, 1, 0, -1],
-        [-1, 1, -1, 1, -1],
-        [-1, 0, 1, -2, -1],
-        [-1, -1, -1, -1, -1]
+        [U, U, U, U, U],
+        [U, U, 1, 0, U],
+        [U, 1, U, 1, U],
+        [U, 0, 1, M, U],
+        [U, U, U, U, U]
     ],
     "safe",
 
     # State 4 is a mine
     [
         [1, 1, 1, 0, 0],
-        [1, -2, 2, 1, 0],
-        [2, 3, -1, 1, 0],
-        [2, -2, 3, 2, 1],
-        [-1, -1, -1, -1, -1]
+        [1, M, 2, 1, 0],
+        [2, 3, U, 1, 0],
+        [2, M, 3, 2, 1],
+        [U, U, U, U, U]
     ],
     "mine",
 
     # State 5 is a mine
     [
         [1, 1, 1, 0, 0],
-        [1, -2, 2, 1, 0],
-        [2, -1, -1, 1, 0],
-        [-1, -1, -1, 1, 0],
+        [1, M, 2, 1, 0],
+        [2, U, U, 1, 0],
+        [U, U, U, 1, 0],
         [0, 0, 0, 0, 0]
     ],
     "mine",
 
     # State 6 is unknown
     [
-        [-1, -1, -1, -1, -1],
-        [-1, -1, -1, -1, -1],
-        [-1, -1, -1, -1, -1],
-        [-1, -1, -1, -1, -1],
-        [-1, -1, -1, -1, -1]
+        [U, U, U, U, U],
+        [U, U, U, U, U],
+        [U, U, U, U, U],
+        [U, U, U, U, U],
+        [U, U, U, U, U]
     ],
     "unknown",
 
     # State 7 is safe
     [
         [0, 0, 0, 0, 0],
-        [0, -1, -2, 1, 0],
-        [0, -1, -1, -1, 0],
-        [0, -1, -1, -1, 0],
+        [0, U, M, 1, 0],
+        [0, U, U, U, 0],
+        [0, U, U, U, 0],
         [0, 0, 0, 0, 0]
     ],
     "safe",
 
     # State 8 is unknown
     [
-        [-1, -1, 1, -1, -1],
-        [-1, 2, -2, 2, -1],
-        [-1, -1, -1, -1, -1],
-        [-1, 2, -1, -1, -1],
-        [-1, -1, -1, -1, -1]
+        [U, U, 1, U, U],
+        [U, 2, M, 2, U],
+        [U, U, U, U, U],
+        [U, 2, U, U, U],
+        [U, U, U, U, U]
     ],
     "unknown",
 
     # State 9 is unknown
     [
-        [1, 2, -1, -1, 2],
-        [1, -2, 3, 3, -2],
-        [1, 2, -1, 2, 1],
-        [1, 3, -1, 3, 1],
-        [1, -2, -2, -2, 1]
+        [1, 2, U, U, 2],
+        [1, M, 3, 3, M],
+        [1, 2, U, 2, 1],
+        [1, 3, U, 3, 1],
+        [1, M, M, M, 1]
     ],
     "unknown"
 ]
